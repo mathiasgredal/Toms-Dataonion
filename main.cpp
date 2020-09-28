52a5eed046f15b581cef25e5d97ef750f35b753e
mathiasgredal <mathiasgredal@icloud.com>
2020-09-28 22:05:09 +0200

    Finished layer 1 and started on layer 2

--- main.cpp
+++ main.cpp
@@ -33,12 +33,11 @@ std::vector<uint8_t> decodeASCII85Chunk(std::string chunk)
     if (chunk.length() != 5)
         throw std::runtime_error("ERROR: incorrect size chunk passed to chunkdecoder");
 
-    int ascii_binary = 0;
-
-    for (size_t i = 0; i < 5; i++) {
-        const int base85 = static_cast<int>(chunk[i]) - 33;
-        ascii_binary += static_cast<int>((base85 * pow(85, 4 - i)));
-    }
+    int ascii_binary = (chunk[0] - 33) * 85 * 85 * 85 * 85
+            + (chunk[1] - 33) * 85 * 85 * 85
+            + (chunk[2] - 33) * 85 * 85
+            + (chunk[3] - 33) * 85
+            + (chunk[4] - 33);
 
     // Extract bits from int using bitshifting
     std::vector<uint8_t> out;
@@ -93,6 +92,63 @@ uint8_t flipEverySecondBit(uint8_t byte)
     return byte ^ 0b01010101;
 }
 
+// Returns true if parity matches and false if it doesnt
+bool calculateParity(uint8_t byte) {
+    int num1s = bitset<8>(byte).count();
+
+    if(num1s % 2 == 0 && bitset<8>(byte)[0] == 0)
+        return true;
+
+    if(num1s % 2 == 1 && bitset<8>(byte)[0] == 1)
+        return true;
+
+    return false;
+}
+
+std::string checkParityAndRemove(std::string input) {
+    // The input should be of size 8
+    if (input.length() != 8)
+        throw std::runtime_error("ERROR: incorrect size chunk passed to paritychecker");
+
+    std::vector<std::pair<uint8_t, bool>> outputMap = {};
+
+    // Go through each byte
+    for(auto c : input) {
+        // Calculate if paritybit matches
+        bool checkParity = calculateParity(c);
+
+        outputMap.push_back({c, checkParity});
+    }
+
+    uint64_t out = 0;
+
+    // Remove the elements where checkparity is false
+    for (int i = 0; i < 8; i++) {
+        //if(outputMap[i].second) {
+            uint64_t temp = outputMap[i].first;
+            temp = temp >> 1;
+
+            temp = temp << 8*(8-i);
+
+            out = out | temp;
+        //}
+    }
+    // Now we need to split the components
+    std::string outstr = "";
+
+    outstr.push_back((out << 0) >> 56);
+    outstr.push_back((out << 8) >> 56);
+    outstr.push_back((out << 16) >> 56);
+    outstr.push_back((out << 24) >> 56);
+    outstr.push_back((out << 32) >> 56);
+    outstr.push_back((out << 40) >> 56);
+    outstr.push_back((out << 48) >> 56);
+
+    std::cout << outstr;
+
+    return outstr;
+}
+
 uint8_t rotateRight(uint8_t byte)
 {
 #pragma clang diagnostic push
@@ -114,26 +170,28 @@ int main()
 
     // Layer 1
     std::string layer1payload = decodeASCII85(readPayload(layer1));
-    //std::cout << readPayload(layer1) << std::endl;
-
-    std::vector<uint8_t> layer2 = {};
-
-    uint8_t test = 0b11111110;
-    uint8_t result = rotateRight(flipEverySecondBit(test));
-
-    std::cout << std::bitset<8>(test) << std::endl;
-    std::cout << std::bitset<8>(flipEverySecondBit(test)) << std::endl;
-    std::cout << std::bitset<8>(result) << std::endl;
+    std::string layer2 = "";
 
     for (auto byte : layer1payload) {
         uint8_t flippedbyte = flipEverySecondBit(byte);
         uint8_t rotatedByte = rotateRight(flippedbyte);
-        std::cout << rotatedByte;
+        layer2.push_back(rotatedByte);
     }
+    std::cout << layer2 << std::endl;
 
-    // Process 1 byte at a time
-    // Flip every second bit using XOR
-    // Rotate 1 bit to the right, setting the last bit on the front
+    // Layer 2
+//    std::string layer2payload = decodeASCII85(readPayload(layer2));
+//    std::string layer3 = "";
+
+
+//    for (size_t i = 0; i < layer2payload.size(); i++) {
+//        std::string chunk = layer2payload.substr(i, 8);
+//        std::string processed = checkParityAndRemove(chunk);
+//        layer3.insert(layer3.end(), processed.begin(), processed.end());
+//    }
+
+
+
 
     return 0;
 }
